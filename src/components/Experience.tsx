d1a17fe91921170055029db35524933952dfb5bd
yurikt13 <bangtantaejunk@gmail.com>
2025-05-04 19:40:44 -0500

    Estructura inicial proyecto

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Experience = () => {
     return (
         <div className="text-white">
-            {/* Idea experiencia */}
-            <p>Experiencia</p>
-            <p>Ademas de la experiencia obtenida en projectos personales, he tenido la oportunidad de desarrollarme laboralmente en las siguientes empresas</p>
-            <div>
-                <p>Unidad de Innovación - UdeA</p>
-                <span>Auxiliar gestión de la innovación</span>
-                <span>2022-2023</span>
-                <div>Qué hice acá?</div>
-            </div>
-            <div>
-                <p>Erco energy</p>
-                <span>Desarroladora backend</span>
-                <span>2023-Actualidad</span>
-                <div>Qué hice acá?</div>
+            <p>Ademas de la experiencia obtenida en projectos personales, he tenido la oportunidad de desarrollarme laboralmente en las siguientes empresas:</p>
+            <div className="flex flex-">
+                <div>
+                    <p>Unidad de Innovación - UdeA</p>
+                    <span>Auxiliar gestión de la innovación</span>
+                    <span>2022-2023</span>
+                    <div>Qué hice acá?</div>
+                </div>
+                <div>
+                    <p>Erco energy</p>
+                    <span>Desarroladora backend</span>
+                    <span>2023-Actualidad</span>
+                    <div>Qué hice acá?</div>
+                </div>
             </div>
         </div>
     )
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,12 @@
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRef } from "react";
 
 interface IParamsShow {
     maxToShow?: number;
     showAll?: boolean;
 }
-const Projects = ({ maxToShow=3, showAll=false }:IParamsShow) => {
+const Projects = ({ maxToShow = 3, showAll = false }: IParamsShow) => {
 
     const data = [
         {
@@ -54,29 +56,52 @@ const Projects = ({ maxToShow=3, showAll=false }:IParamsShow) => {
     const displayedProjects = showAll ? data : data.slice(0, maxToShow);
 
     return (
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-            {displayedProjects.map((project) => (
-                <div
-                    key={project.id}
-                    className="w-full flex flex-col border border-primary-100 rounded-2xl bg-primary-50 p-4 gap-4"
-                >
-                    <p className="text-white text-2xl font-bold">{project.title}</p>
-                    <img
-                        src={project.image}
-                        alt={project.title}
-                        className="w-full h-32 object-cover rounded-2xl"
-                    />
-                    <p>{project.description}</p>
-                    <div className="flex flex-wrap gap-2">
-                        {project.tags.map((tag, index) => (
-                            <span
-                                key={index}
-                                className="bg-primary-100 text-primary-500 px-2 py-1 rounded-full text-xs font-semibold"
-                            >{tag}</span>
-                        ))}
+        <div className="relative w-full">
+
+            {!showAll && (
+                <>
+                    <button
+                        className="absolute left-2 top-1/2 z-10 transform -translate-y-1/2 bg-primary-500 text-primary-100 border border-primary-100 rounded-full p-2 hover:bg-primary-100 hover:text-primary-500 transition"
+                        onClick={() => scroll('left')}
+                    >
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                    </button>
+                    <button
+                        className="absolute right-2 top-1/2 z-10 transform -translate-y-1/2 bg-primary-500 text-primary-100 border border-primary-100 rounded-full p-2 hover:bg-primary-100 hover:text-primary-500 transition"
+                        onClick={() => scroll('right')}
+                    >
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                </>
+            )}
+
+            <div
+                ref={scrollRef}
+                className={`w-full overflow-x-auto no-scrollbar ${showAll ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4' : 'flex gap-4 py-4 px-2'}`}
+            >
+                {displayedProjects.map((project) => (
+                    <div
+                        key={project.id}
+                        className="w-full flex flex-col border border-primary-100 rounded-2xl bg-primary-50 p-4 gap-4"
+                    >
+                        <p className="text-white text-2xl font-bold">{project.title}</p>
+                        <img
+                            src={project.image}
+                            alt={project.title}
+                            className="w-full h-32 object-cover rounded-2xl"
+                        />
+                        <p>{project.description}</p>
+                        <div className="flex flex-wrap gap-2">
+                            {project.tags.map((tag, index) => (
+                                <span
+                                    key={index}
+                                    className="bg-primary-100 text-primary-500 px-2 py-1 rounded-full text-xs font-semibold"
+                                >{tag}</span>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
